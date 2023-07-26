e1b505cbef351e2a4b3b1620d658cdd2f4ef31cf
Junseo Park <junseo_park@tmax.co.kr>
2023-07-26 18:36:30 +0900

    feat: impl constant class

---
+++ lib/foundation/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Constants {
+  const Constants._();
+
+  factory Constants() {
+    return const Constants._();
+  }
+
+  static final Constants instance = Constants();
+}
