a49f78dd741e53f8e4669baa20bfbf891238a9e9
Junseo <66871265+blan19@users.noreply.github.com>
2023-07-29 14:06:28 +0900

    feat: init firebase (#16)
    * feat: add firebase library
    
    * chore: update gitignore
    
    * config: set firebase config
    
    * feat: set firebase default config key

--- lib/foundation/constants.dart
+++ lib/foundation/constants.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 @immutable
 class Constants {
-  const Constants._();
+  const Constants._({
+    required this.firebaseProjectID,
+    required this.firebaseStorageBucket,
+    required this.firebaseMessagingSenderId,
+    required this.firebaseAndroidApiKey,
+    required this.firebaseAndroidAppId,
+    required this.firebaseIosApiKey,
+    required this.firebaseIosAppId,
+    required this.firebaseIosBundleId,
+    required this.firebaseIosClientId,
+  });
 
   factory Constants() {
-    return const Constants._();
+    return Constants._(
+      firebaseProjectID: dotenv.env['FIREBASE_PROJECT_ID']!,
+      firebaseStorageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      firebaseMessagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      firebaseIosApiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+      firebaseIosAppId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+      firebaseIosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+      firebaseIosClientId: dotenv.env['FIREBASE_IOS_CLIENT_ID']!,
+      firebaseAndroidApiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+      firebaseAndroidAppId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+    );
   }
 
   static final Constants instance = Constants();
+
+  final String firebaseProjectID;
+  final String firebaseStorageBucket;
+  final String firebaseMessagingSenderId;
+
+  final String firebaseIosApiKey;
+  final String firebaseIosAppId;
+  final String firebaseIosClientId;
+  final String firebaseIosBundleId;
+
+  final String firebaseAndroidApiKey;
+  final String firebaseAndroidAppId;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'dart:async';
 
 import 'package:diary/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
-  await dotenv.load(fileName: ".env");
-  runZonedGuarded(
-    () {
-      runApp(const ProviderScope(child: App()));
-    },
-    (error, stack) {
-      print(error);
-    },
-  );
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // env load
+    await dotenv.load(fileName: ".env");
+    // Firebase
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    runApp(const ProviderScope(child: App()));
+  }, (error, stack) {
+    print(error);
+  });
 }
